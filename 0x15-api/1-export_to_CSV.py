8c4478deb312073548d0c8e8542e435374d81593
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-04-05 21:20:19 -0700

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+
+if len(sys.argv) > 1:
+    user_id = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(user_id)).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                         .format(user_id)).json()
+
+    with open('{}.csv'.format(user_id), 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([user_id, user.get('name'), task
+                             .get('completed'), task.get('title')])
